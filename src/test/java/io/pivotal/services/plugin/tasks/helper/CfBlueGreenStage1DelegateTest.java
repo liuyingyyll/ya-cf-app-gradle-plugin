003cce7163cce01f4481610bcd85be1ce3b773d2
Biju Kunjummen <biju.kunjummen@gmail.com>
2016-09-16 17:10:57 -0700

    Fixed failing test

--- src/test/java/io/pivotal/services/plugin/tasks/helper/CfBlueGreenStage1DelegateTest.java
+++ src/test/java/io/pivotal/services/plugin/tasks/helper/CfBlueGreenStage1DelegateTest.java
@@ -3,6 +3,7 @@ package io.pivotal.services.plugin.tasks.helper;
 
 import io.pivotal.services.plugin.CfAppProperties;
 import org.cloudfoundry.operations.CloudFoundryOperations;
+import org.cloudfoundry.operations.applications.ApplicationDetail;
 import org.gradle.api.Project;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,16 +11,24 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 public class CfBlueGreenStage1DelegateTest {
 
 	@Mock
 	private CfPushDelegate pushDelegate;
+
+	@Mock
+	private CfAppDetailsDelegate appDetailsDelegate;
 
 	@InjectMocks
 	private CfBlueGreenStage1Delegate blueGreenStage1Delegate;
@@ -30,7 +39,7 @@ public class CfBlueGreenStage1DelegateTest {
 	}
 
 	@Test
-	public void testStage1() {
+	public void testStage1ExistingApp() {
 		Project project = mock(Project.class);
 		CloudFoundryOperations cfOperations = mock(CloudFoundryOperations.class);
 		CfAppProperties cfAppProperties = CfAppProperties
@@ -38,6 +47,17 @@ public class CfBlueGreenStage1DelegateTest {
 				.name("test")
 				.hostName("route")
 				.build();
+
+		when(appDetailsDelegate.getAppDetails(any(CloudFoundryOperations.class), eq(cfAppProperties)))
+				.thenReturn(Mono.just(Optional.of(ApplicationDetail.builder().
+						name("test").instances(2)
+						.runningInstances(2)
+						.stack("stack")
+						.diskQuota(500)
+						.memoryLimit(100)
+						.requestedState("start")
+						.id("id")
+						.build())));
 
 		this.blueGreenStage1Delegate.runStage1(project, cfOperations, cfAppProperties);
 

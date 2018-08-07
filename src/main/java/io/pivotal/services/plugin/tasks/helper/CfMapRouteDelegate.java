08a108be0e16ced63940a783e3ff90a5512893b2
Gabriel <gmcouto@gmail.com>
2018-08-07 03:13:12 -0300

    map and unmap tasks changed to new routing logic

--- src/main/java/io/pivotal/services/plugin/tasks/helper/CfMapRouteDelegate.java
+++ src/main/java/io/pivotal/services/plugin/tasks/helper/CfMapRouteDelegate.java
@@ -1,7 +1,11 @@
 package io.pivotal.services.plugin.tasks.helper;
 
+import java.util.List;
+
+import io.pivotal.services.plugin.CfManifestUtil;
 import io.pivotal.services.plugin.CfProperties;
 import org.cloudfoundry.operations.CloudFoundryOperations;
+import org.cloudfoundry.operations.applications.DecomposedRoute;
 import org.cloudfoundry.operations.routes.MapRouteRequest;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
@@ -18,20 +22,37 @@ public class CfMapRouteDelegate {
 
     public Mono<Void> mapRoute(CloudFoundryOperations cfOperations,
                                CfProperties cfProperties) {
-
-        Mono<Void> resp = cfOperations.routes()
-            .map(MapRouteRequest
-                .builder()
-                .applicationName(cfProperties.name())
-                .domain(cfProperties.domain())
-                .host(cfProperties.host())
-                .path(cfProperties.path()).build()).then().doOnSubscribe((s) -> {
-                LOGGER.lifecycle("Mapping hostname '{}' in domain '{}' with path '{}' of app '{}'", cfProperties.host(),
-                    cfProperties.domain(), cfProperties.path(), cfProperties.name());
-            });
+        Mono<Void> resp = Mono.empty();
+        if (cfProperties.routes() != null && !cfProperties.routes().isEmpty()) {
+            List<DecomposedRoute> routes = CfManifestUtil.decomposedRoutes(cfProperties.routes());
+            for(DecomposedRoute route: routes) {
+                resp = resp.then(cfOperations.routes()
+                    .map(MapRouteRequest
+                        .builder()
+                        .applicationName(cfProperties.name())
+                        .domain(route.getDomain())
+                        .host(route.getHost())
+                        .path(route.getPath())
+                        .build()).then().doOnSubscribe((s) -> {
+                        LOGGER.lifecycle("Mapping hostname '{}' in domain '{}' with path '{}' of app '{}'", route.getHost(),
+                            route.getDomain(), route.getPath(), cfProperties.name());
+                    }));
+            }
+        } else {
+            resp = resp.then(cfOperations.routes()
+                .map(MapRouteRequest
+                    .builder()
+                    .applicationName(cfProperties.name())
+                    .domain(cfProperties.domain())
+                    .host(cfProperties.host())
+                    .path(cfProperties.path())
+                    .build()).then().doOnSubscribe((s) -> {
+                    LOGGER.lifecycle("Mapping hostname '{}' in domain '{}' with path '{}' of app '{}'", cfProperties.host(),
+                        cfProperties.domain(), cfProperties.path(), cfProperties.name());
+                }));
+        }
 
         return resp;
-
     }
 
 }
--- src/main/java/io/pivotal/services/plugin/tasks/helper/CfUnMapRouteDelegate.java
+++ src/main/java/io/pivotal/services/plugin/tasks/helper/CfUnMapRouteDelegate.java
@@ -1,7 +1,11 @@
 package io.pivotal.services.plugin.tasks.helper;
 
+import java.util.List;
+
+import io.pivotal.services.plugin.CfManifestUtil;
 import io.pivotal.services.plugin.CfProperties;
 import org.cloudfoundry.operations.CloudFoundryOperations;
+import org.cloudfoundry.operations.applications.DecomposedRoute;
 import org.cloudfoundry.operations.routes.UnmapRouteRequest;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
@@ -17,20 +21,35 @@ public class CfUnMapRouteDelegate {
     private static final Logger LOGGER = Logging.getLogger(CfUnMapRouteDelegate.class);
 
     public Mono<Void> unmapRoute(CloudFoundryOperations cfOperations, CfProperties cfProperties) {
-
-        return cfOperations.routes()
-            .unmap(UnmapRouteRequest
-                .builder()
-                .applicationName(cfProperties.name())
-                .domain(cfProperties.domain())
-                .host(cfProperties.host())
-                .path(cfProperties.path())
-                .build()).doOnSubscribe((s) -> {
-                LOGGER.lifecycle("Unmapping hostname '{}' in domain '{}' with path '{}' of app '{}'", cfProperties.host(),
-                    cfProperties.domain(), cfProperties.path(), cfProperties.name());
-            });
-
-
+        Mono<Void> resp = Mono.empty();
+        if (cfProperties.routes() != null && !cfProperties.routes().isEmpty()) {
+            List<DecomposedRoute> routes = CfManifestUtil.decomposedRoutes(cfProperties.routes());
+            for(DecomposedRoute route: routes) {
+                resp = resp.then(cfOperations.routes()
+                    .unmap(UnmapRouteRequest
+                        .builder()
+                        .applicationName(cfProperties.name())
+                        .domain(route.getDomain())
+                        .host(route.getHost())
+                        .path(route.getPath())
+                        .build()).then().doOnSubscribe((s) -> {
+                        LOGGER.lifecycle("Unmapping hostname '{}' in domain '{}' with path '{}' of app '{}'", route.getHost(),
+                            route.getDomain(), route.getPath(), cfProperties.name());
+                    }));
+            }
+        } else {
+            resp = cfOperations.routes()
+                .unmap(UnmapRouteRequest
+                    .builder()
+                    .applicationName(cfProperties.name())
+                    .domain(cfProperties.domain())
+                    .host(cfProperties.host())
+                    .path(cfProperties.path())
+                    .build()).doOnSubscribe((s) -> {
+                    LOGGER.lifecycle("Unmapping hostname '{}' in domain '{}' with path '{}' of app '{}'", cfProperties.host(),
+                        cfProperties.domain(), cfProperties.path(), cfProperties.name());
+                });
+        }
+        return resp;
     }
-
 }
